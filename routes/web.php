eb0a9796c673e224ff596e30df51c1dfda60a925
magang 5 UDI <fauziardiansyah302@gmail.com>
2024-10-08 12:26:19 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,8 @@ use App\Http\Controllers\LoginController;
 Route::get('/', [LoginController::class, 'index'])->name('login');
 Route::post('/actionLogin', [LoginController::class, 'actionLogin'])->name('actionLogin');
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::post('/password_update', [DashboardController::class, 'UpdatePassword'])->name('UpdatePassword');
-Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
-// Route::middleware(['auth'])->group(function () {
-// });
+Route::middleware(['auth'])->group(function () {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+});
 
